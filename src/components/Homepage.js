1ac7694cd116429072ba888dba3a4f772fcadec7
johnny <johnny1194@hotmail.com>
2021-09-10 10:37:13 -0400

    ading component homepage

---
+++ src/components/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import "../styles/HomePage.css"
+
+function HomePage() {
+    return (
+        <div className="home-page-container">
+            <h2 className="home-page-header">Home Page</h2><br/>
+            <h1>Gamester</h1><br/>
+            <p>Are you interesting in becoming a famous videogame player and show others what you can do?</p>
+            <p>Do you want to see others your favorite players, playing the games you like?</p>
+            <p>You came to the right place to do it!!</p>
+            <p>Welcome to Gamester app the v ideogame platform to watch videogame players with their games</p>
+            <img href="../images/G.png" alt="logo"></img>
+            <p>Copyright:</p>
+            <p>© 2021 Gamester, Inc</p>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/constrains/index.js
@@ -0,0 +1,1 @@
+const BASE_URL = "https://gamester-app-backend.herokuapp.com/"
