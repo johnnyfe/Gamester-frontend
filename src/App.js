8077a8a209760aa8fc4d0aeed1f95d64c5b0cdbe
johnny <johnny1194@hotmail.com>
2021-09-10 22:43:47 -0400

    adding navbar component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import HomePage from './components/Homepage';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import PlayerContainer from './components/PlayerContainer';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="app-container"></div>
+      <NavBar />
       <Switch>
         <Route exact path="/">
               <HomePage />
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+// import '../styles/NavBar.css';
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <Link to="/">HomePage</Link>
+            <Link to="/players">All Players</Link>
+            <Link to="/games">All Games</Link>
+            <Link to="/gameplays">All Gameplays</Link>
+        </div>
+    );
+}
+
+export default NavBar;
