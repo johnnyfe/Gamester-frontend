c776f47c3f6e60718434bdd64ec7dbbc07496400
johnny <johnny1194@hotmail.com>
2021-09-13 12:29:01 -0400

    adding homepage style

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,17 +1,19 @@
 import React from 'react';
-// import "../styles/HomePage.css"
+import "../styles/HomePage.css"
 
 function HomePage() {
     return (
         <div className="home-page-container">
-            <h2 className="home-page-header">Home Page</h2><br/>
             <h1>Gamester</h1><br/>
+            <div className="home-page-content">
+            <img src="https://images2.alphacoders.com/141/141972.jpg" alt="homepage-videogame-characters"></img>
             <p>Are you interesting in becoming a famous videogame player and show others what you can do?</p>
-            <p>Do you want to see others your favorite players, playing the games you like?</p>
-            <p>You came to the right place to do i!!</p>
-            <p>Welcome to Gamester app the v ideogame platform to watch videogame players with their games</p>
+            <p>Do you want to see your favorite players, playing the games you like?</p>
+            <p>You came to the right place to do it!!</p>
+            <p>Welcome to Gamester app the videogame platform to watch videogame players with their games</p><br/>
             <p>Copyright:</p>
             <p>© 2021 Gamester, Inc</p>
+            </div>
         </div>
     );
 }
